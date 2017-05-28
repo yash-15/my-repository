81d7cb6503dc1a58c264e58ed918c13a2e5350b5
yash-15 <yash.15296@gmail.com>
2017-05-28 15:35:06 +0000

    checks every block for best configuration by rotating score 54.84

--- tetris.cpp
+++ tetris.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 
 #define N 225005
-#define M 155
+int M =0;
 #define inf 10000000
-int g[N][M];
 vector < int> base;
-vector < vector<int> > x,y;
+vector < vector<int> > x,y,g;
 int X,Y;
 void rotate(int i){
 	int n=x[i].size();
 	swap(x[i][0],y[i][0]);
 	for(int j=1;j<n;j++){
 		swap(x[i][j],y[i][j]);
-		x[i][j]=x[i][0]-x[i][j];
-	}
+		x[i][j]=x[i][0]-x[i][j]+1;
+	}	
 }
 
 void move(int i, int dx, int dy){
@@ -34,21 +33,34 @@ void push(int i, int val){
 }
 
 void align(int i){
-	int n=x[i].size()-1,pos,act=0,dy=0;
+	int n=x[i].size()-1,pos,ht,cmp=inf+inf,act=0,dy=0,rot=0;
+	vector<int> mn;
+	for(int c=0;c<4;c++){
+		pull(i,inf);
+		mn.clear();
+		mn.resize(1+y[i][0],inf+inf);
+		for(int j=1;j<=n;j++)
+			mn[y[i][j]]=min(mn[y[i][j]],x[i][j]);
+		for(int rt=0;y[i][0]+rt<=M;rt++){
+			pos=inf+inf;ht=0;
+			for(int j=1;j<=y[i][0];j++)
+				pos=min(pos,mn[j]-base[j+rt]-1);
 
-	vector<int> mn(y[i][0]+1,inf+inf);
-	for(int j=1;j<=n;j++)
-		mn[y[i][j]]=min(mn[y[i][j]],x[i][j]);
-	for(int rt=0;y[i][0]+rt<M;rt++){
-		pos=inf;
-		for(int j=1;j<=y[i][0];j++)
-			pos=min(pos,mn[j]-base[j+rt]-1);
-		if(pos>act){
-			act=pos;
-			dy=rt;
+			ht=x[i][0]-pos;
+			if(ht<cmp){
+				act=pos;
+				dy=rt;
+				rot=c;
+				cmp=ht;
+			}
 		}
+		push(i,inf);
+		rotate(i);
 	}
-	move(i,-act,dy);
+	//cout<<"min ht "<<ht<<" with "<<rot<<" rots and "<<dy<<" right "<<act<<" down\n";
+	while(rot--)
+		rotate(i);
+	move(i,inf-act,dy);
 }
 
 void fix(int i){
@@ -61,9 +73,14 @@ void fix(int i){
 	}
 }
 
+void show(){
+	for(int i=1;i<=X;i++,puts(""))
+		for(int j=1;j<=Y;j++)
+			printf("%d ",g[i][j]);
+}
+
 int main(){
 	int n,sz;
-	base.resize(M);
 	scanf("%d",&n);
 	x.resize(n+1);
 	y.resize(n+1);
@@ -77,15 +94,17 @@ int main(){
 			x[i][0]=max(x[i][0],x[i][j]);
 			y[i][0]=max(y[i][0],y[i][j]);
 		}
-
-		if(x[i][0]>y[i][0])
-			rotate(i);
-		pull(i,inf);
+		M=max(M,max(x[i][0],y[i][0]));
+	}
+	g.resize(M*n+1);
+	base.resize(M+1);
+	for(int i=0;i<g.size();i++)
+		g[i].resize(M+1);
+	for(int i=1;i<=n;i++){
 		align(i);
 		fix(i);
+		// show();
 	}
 	cout<<X<<" "<<Y<<"\n";
-	for(int i=1;i<=X;i++,puts(""))
-		for(int j=1;j<=Y;j++)
-			printf("%d ",g[i][j]);
+	show();
 }
